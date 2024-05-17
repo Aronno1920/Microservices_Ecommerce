19ea3e53d3b71798aac408f048202487bcf9e92f
Selim Ahmed <aronno1920@hotmail.com>
2024-05-17 15:19:28 +0600

    Add Model and get value from connection string
    1. Install a package MongoRepo
    2. Craete Product Model
    3. Get MongoDB connectionStrinng and databaseName

---
+++ src/MicroservicesEcom/Catelog.API/Context/CatalogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MongoRepo.Context;
+
+namespace Catelog.API.Context
+{
+    public class CatalogDbContext : ApplicationDbContext
+    {
+        static IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json",true,true).Build();
+
+        static string connectionString = configuration.GetConnectionString("Catalog.API");
+        static string databaseName = configuration.GetValue<string>("DatabaseName");
+
+        public CatalogDbContext() : base(connectionString, databaseName)
+        {
+
+        }
+    }
+}
---
+++ src/MicroservicesEcom/Catelog.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catelog.API.Models
+{
+    public class Product
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Name { get; set; }  
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Summary {  get; set; }
+        public string ImageFile { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
