38a90af11ec3fe15c5075d9a2b2a98d70c7336f2
Selim Ahmed <aronno1920@hotmail.com>
2024-05-18 17:13:18 +0600

    Get custom response from API using new extension CoreApiResponse

--- src/MicroservicesEcom/Catelog.API/Controllers/CatalogController.cs
+++ src/MicroservicesEcom/Catelog.API/Controllers/CatalogController.cs
@@ -1,5 +1,6 @@
 ﻿using Catelog.API.Interfaces.Manager;
 using Catelog.API.Models;
+using CoreApiResponse;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -8,18 +9,20 @@ namespace Catelog.API.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class CatalogController : ControllerBase
+    public class CatalogController : BaseController
     {
         IProductManager _productManager;
-        public CatalogController(IProductManager productManager) {
+        public CatalogController(IProductManager productManager)
+        {
             _productManager = productManager;
         }
 
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
-        public IActionResult GetProducts() {
+        //[ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public IActionResult GetProducts()
+        {
             var products = _productManager.GetAll();
-            return Ok(products);
+            return CustomResult(products);
         }
     }
 }
