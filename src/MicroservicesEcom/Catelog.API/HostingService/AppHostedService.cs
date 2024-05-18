4fdc374ecb966389139765c8e4f126c0b8e46a51
Selim Ahmed <aronno1920@hotmail.com>
2024-05-18 15:34:23 +0600

    Inplement App Hosted |Service
    Hosted service is run only one time at Start application and Stop application.

---
+++ src/MicroservicesEcom/Catelog.API/HostingService/AppHostedService.cs
@@ -0,0 +1,15 @@
+﻿namespace Catelog.API.HostingService
+{
+    public class AppHostedService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
