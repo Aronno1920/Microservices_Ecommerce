06bfe6e31b0db1a05486fe13b2f94169664a91f5
Selim Ahmed <aronno1920@hotmail.com>
2024-05-19 16:49:18 +0600

    Create models for Basket.API

---
+++ src/MicroservicesEcom/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart() { }
+        public ShoppingCart(String userName)
+        {
+            this.UserName = userName;
+        }
+
+        public String UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }=new List<ShoppingCartItem>();
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = 0;
+                foreach (ShoppingCartItem item in Items)
+                {
+                    total += item.Price;
+                }
+                return total;
+            }
+        }
+
+    }
+}
