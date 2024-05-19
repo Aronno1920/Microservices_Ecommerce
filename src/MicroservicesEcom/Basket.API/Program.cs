0abd2729d358afa1b1d2a516d9c26d503d9d7709
Selim Ahmed <aronno1920@hotmail.com>
2024-05-19 17:01:20 +0600

    Configure Redis database with .net

--- src/MicroservicesEcom/Basket.API/Program.cs
+++ src/MicroservicesEcom/Basket.API/Program.cs
@@ -1,8 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Configure Redis Database
+builder.Services.AddStackExchangeRedisCache(options => 
+{
+    options.Configuration = builder.Configuration.GetConnectionString("BasketDB");
+});
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
