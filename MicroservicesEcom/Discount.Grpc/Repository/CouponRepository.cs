7071179fccf7825811d8cac7e29344570d5ba2aa
Selim Ahmed <aronno1920@hotmail.com>
2024-05-23 01:14:32 +0600

    Create model & repository
    We can create model or copy-paste from Discount.APi project and simillar activity for repository interface and class. If we copy-paste from Discount.API project, we must change namespace for those model,interface and class

---
+++ MicroservicesEcom/Discount.Grpc/Repository/CouponRepository.cs
@@ -0,0 +1,76 @@
+﻿using Dapper;
+using Discount.Grpc.Models;
+using Npgsql;
+
+namespace Discount.Grpc.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        IConfiguration _configuration;
+
+        public CouponRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            var connection = new NpgsqlConnection(_configuration.GetConnectionString("DiscountDb"));
+
+            var affedRow = await connection.ExecuteAsync("INSERT INTO Coupon(productId,productName,description,amount) VALUES (@ProductId, @ProductName, @Description, @Amount)", new { ProductId = coupon.ProductId, ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+            if (affedRow > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteDiscount(string productId)
+        {
+            var connection = new NpgsqlConnection(_configuration.GetConnectionString("DiscountDb"));
+
+            var affedRow = await connection.ExecuteAsync("DELETE FROM Coupon WHERE productId=@ProductId",new { ProductId = productId});
+            if (affedRow > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<Coupon> GetDiscount(string productId)
+        {
+            var connection = new NpgsqlConnection(_configuration.GetConnectionString("DiscountDb"));
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>("SELECT * FROM Coupon WHERE productId=@ProductId", new { ProductId = productId });
+            if (coupon == null)
+            {
+                return new Coupon() { Amount = 0, ProductName = "No Discount" };
+            }
+            else
+            {
+                return coupon;
+            }
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            var connection = new NpgsqlConnection(_configuration.GetConnectionString("DiscountDb"));
+
+            var affedRow = await connection.ExecuteAsync("UPDATE Coupon SET productId=@ProductId,productName=@ProductName,description=@Description,amount=@Amount", new { ProductId = coupon.ProductId, ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+            if (affedRow > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MicroservicesEcom/Discount.Grpc/Repository/ICouponRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Grpc.Models;
+
+namespace Discount.Grpc.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<Coupon> GetDiscount(String productId);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(String productId);
+    }
+}
