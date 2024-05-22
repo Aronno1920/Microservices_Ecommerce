3806500b7a576a5591f632907de2d63a8fbc5134
Selim Ahmed <aronno1920@hotmail.com>
2024-05-22 23:43:41 +0600

    Implementing a Repository Pattern with PostgreSQL in .NET Core

---
+++ MicroservicesEcom/Discount.API/Repository/ICouponRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.API.Models;
+
+namespace Discount.API.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<Coupon> GetDiscount(String productId);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(String productId);
+    }
+}
