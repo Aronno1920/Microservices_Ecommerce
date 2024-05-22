50865142bd9c23dbf94d22996449691bffae0852
Selim Ahmed <aronno1920@hotmail.com>
2024-05-23 01:54:27 +0600

    Implement GetDiscount gRPC mothed

---
+++ MicroservicesEcom/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,31 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repository;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    {
+        ICouponRepository _couponRepository;
+        ILogger<DiscountService> _logger;
+
+        public DiscountService(ICouponRepository couponRepository, ILogger<DiscountService> logger)
+        {
+            _couponRepository = couponRepository;
+            _logger = logger;
+        }
+
+        public override async Task<CouponRequest> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _couponRepository.GetDiscount(request.ProductId);
+
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "No discount found for the prodect"));
+            }
+            _logger.LogInformation("Discount is retived for {productName} with amount {amount}",coupon.ProductName,coupon.Amount);
+
+            return new CouponRequest { ProductId = coupon.ProductId, ProductName=coupon.ProductName, Description=coupon.Description,Amount=coupon.Amount};
+        }
+    }
+}
