17e122b7746cb0481e5dd6d0de383e3b4a3aae56
Selim Ahmed <aronno1920@hotmail.com>
2024-05-31 17:42:14 +0600

    Install Dependency

---
+++ MicroservicesEcom/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersByUserName/GetOrdersByUserQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersByUserName
+{
+    public class GetOrdersByUserQuery:IRequest<List<OrderVm>>
+    {
+    }
+}
