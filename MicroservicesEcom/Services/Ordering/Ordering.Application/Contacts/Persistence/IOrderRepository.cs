58d477a7938bedbe9ece6125bb91d5b01148d2f5
Selim Ahmed <aronno1920@hotmail.com>
2024-05-31 17:20:17 +0600

    Model created for email

---
+++ MicroservicesEcom/Services/Ordering/Ordering.Application/Contacts/Persistence/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using EF.Core.Repository.Interface.Repository;
+using Ordering.Domain.Models;
+
+
+namespace Ordering.Application.Contacts.Persistence
+{
+    public interface IOrderRepository:ICommonRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrderByUserName(string UserName);
+    }
+}
