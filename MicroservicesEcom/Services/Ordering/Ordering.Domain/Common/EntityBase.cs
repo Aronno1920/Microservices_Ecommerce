b1aa6c8896e09fab03a0c75d54184c85da892266
Selim Ahmed <aronno1920@hotmail.com>
2024-05-31 16:29:45 +0600

    Update Model

--- MicroservicesEcom/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
+++ MicroservicesEcom/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -9,8 +9,8 @@ namespace Ordering.Domain.Common
     public abstract class EntityBase
     {
         public string CreatedBy { get; set; } 
-        public DateTime CreatedDate { get; set; }
-        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string UpdatedBy { get; set; } = String.Empty;
         public DateTime? UpdatedDate { get; set; }
     }
 }
