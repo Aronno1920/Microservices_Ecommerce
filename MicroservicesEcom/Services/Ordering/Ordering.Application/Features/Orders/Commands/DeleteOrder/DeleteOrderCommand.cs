4e0ce670524e0a3f07ab1cd385e66b3d6e926d45
Selim Ahmed <aronno1920@hotmail.com>
2024-06-09 17:40:01 +0600

    Implement Delete Order

---
+++ MicroservicesEcom/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommand:IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
