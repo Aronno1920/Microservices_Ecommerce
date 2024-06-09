680aaac2fd84dcac3650cef0cbae161a7e3f8319
Selim Ahmed <aronno1920@hotmail.com>
2024-06-09 16:53:22 +0600

    Introduce FluentValidation

---
+++ MicroservicesEcom/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandValidation.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+
+namespace Ordering.Application.Features.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandValidation:AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidation()
+        {
+            RuleFor(c => c.UserName)
+                .NotEmpty().WithMessage("Please enter user name.")
+                .EmailAddress().WithMessage("User name should be valid email.");
+
+            RuleFor(c => c.FirstName)
+                .NotEmpty().WithMessage("Please enter first name.");
+
+            RuleFor(c => c.TotalPrice)
+                .GreaterThan(0).WithMessage("Total amount should be greater then zero.");
+
+            RuleFor(c => c.EmailAddress)
+                .NotEmpty().WithMessage("Please enter email.")
+                .EmailAddress().WithMessage("Email should be valid email.");
+
+        }
+    }
+}
