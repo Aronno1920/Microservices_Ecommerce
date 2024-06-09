5f9d1297d91e91c2c4567aaa64bdc9ab70da55b6
Selim Ahmed <aronno1920@hotmail.com>
2024-06-09 17:14:57 +0600

    Update Order Command

---
+++ MicroservicesEcom/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommand:IRequest<bool>
+    { 
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        // Billing Address
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        //Payment
+        public string CardType { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string Expiration { get; set; }
+        public string PaymentMethod { get; set; }
+        public string UpdatedBy { get; set; } = String.Empty;
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
